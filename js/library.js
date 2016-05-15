8a4c32d4376f2b0c306ac4fefdc6c5a3140b1357
Khánh Nguyễn <Khnh Nguyn>
2016-05-15 18:10:18 +0300

    added search bar

--- js/library.js
+++ js/library.js
@@ -4,4 +4,12 @@ $(document).ready(function(){
 		autoplaySpeed:2000,
 		arrows:true
 	});
-});
+	$('#searchBar').submit(function(event){
+		event.preventDefault();
+		var searchArgu = $('input[name="search"]').val();
+		// searchArgu.replace(" ","+");
+		var url = "https://metcat.amkit.fi/vwebv/search?searchArg="+searchArgu+"&searchCode=GKEY%5E*&limitTo=none&recCount=10&searchType=1&page.search.search.button=Hae"
+		console.log("submitted");
+		window.location.href = url;
+	});
+})
